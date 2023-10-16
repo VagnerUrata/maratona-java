4b73fcd95e6cd8117fe3e3728e2f23a4c54cdfc1
vagner <vagnerurata@gmail.com>
2023-10-16 18:09:54 -0300

    Sobrecarga de construtores

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstutores/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Dconstutores.test;
+
+import academy.devdojo.maratonajava.javacore.Dconstutores.domino.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime( "Pokemon", "TV", 12, "Fantasia", "Confort");
+        anime.imprime();
+    }
+}
